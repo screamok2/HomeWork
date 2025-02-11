2a5a24ab3716af018bb2c355c943ef887b66ef45
screa <screamok2@gmail.com>
2025-02-11 21:07:26 +0100

    11.02.2025

--- HomeWork.py
+++ HomeWork.py
@@ -1,6 +1,9 @@
-number = input("input your number:")
-list(number)
-print(list(number))
-print(number[0])
-print(number[1])
+number = int(input('input number'))
+
+print(number // 1000)
+print(number // 100 % 10 )
+print(number // 10 % 10  )
+print(number % 10 )
+
+
 
