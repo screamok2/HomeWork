26bd9c60227c4b96d7e3986b000cbc0df2d2323b
screa <screamok2@gmail.com>
2025-02-11 23:00:56 +0100

    HomeWork lesson 2

--- HomeWork.py
+++ HomeWork.py
@@ -1,3 +1,15 @@
+# перший варіант
+
+number = input("input your number:")
+list(number)
+print(list(number))
+print(number[0])
+print(number[1])
+print(number[2])
+print(number[3])
+
+#другий варіант
+
 number = int(input('input number'))
 
 print(number // 1000)
